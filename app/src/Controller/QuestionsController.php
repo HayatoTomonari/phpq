23ed7a5f93bb957426daf4e71134b197b78a9046
HayatoTomonari <w81026pc@yahoo.co.jp>
2019-04-27 11:19:42 +0900

    初めてのgithub

---
+++ app/src/Controller/QuestionsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+/**
+* Questions Controller
+*/
+class  QuestionsController extends AppController
+{
+
+/**
+* @inheritdoc
+*/
+  public function initialize()
+  {
+    parent::initialize();
+  }
+
+/**
+*質問一覧画面
+*
+* @return \Cake\Http\Response|void
+*/
+
+  public function index()
+  {
+    //ここに処理を記述する。
+  }
+}
